727c443b6271db7f04145bff63f026b7fb5d52a4
Teyeb <hussenab@gmail.com>
2022-06-04 23:27:08 +0300

    order

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,7 +14,7 @@ class User extends Authenticatable
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
-     * The attributes thatt are mass assignable.
+     * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
